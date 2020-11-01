d39503ee826aafb01575a47968f2596ea69702fb
Qishilu622 <72714772+Qishilu622@users.noreply.github.com>
2020-11-01 22:55:52 +0100

    Update jj.php
    functional version

--- jj.php
+++ jj.php
@@ -1,49 +1,52 @@
-  <?php
+<?php
         $quiz = array(
             "Q1" => array(
                 "question"=>"Máte rád sushi?",
                 "options" => array(
-                    "Ano",
-                    "Ne",
-                    "Nevím"
+                   "Ano" => 'Ano',
+                   "Ne" => 'Ne',
+                   "Nevím" => 'Nevím'
                 ),
             ),
-
+ 
            "q2" => array(
-                "question"=>"Kolik máte dětí",
+                "question"=>"Kolik máte dětí?",
                 "options" => array(
-                    "Jedno",
-                    "Dvě",
-                    "Tři"),
+                    "Jedno" => 'Jedno',
+                    "Dvě" => 'Dvě',
+                    "Tři" => 'Tři'),
             ),
-
+ 
             "q3" =>array(
-            "question"=>"Máte zlé sny",
+            "question"=>"Máte zlé sny?",
             "options" => array(
-                "Často",
-                "Občas",
-                "Nikdy"
+                "Často" => 'Často',
+                "Občas" => 'Občas',
+                "Nikdy" => 'Nikdy'
             ),
             )
         );
-
-
-
         echo "<form name='quiz' method='post'>";
-
         foreach( $quiz as $index => $arr ){
-
             $question=$arr['question'];
             $options=$arr['options'];
-
-            echo '<h3>Question: '.$index.': '.$question.'</h3>';
+            echo $question.'</h3>';
             echo '<ul>';
             foreach( $options as $i => $option ) 
-            echo "<li><input type='radio' name='{$index}[]' value='{$i}'/>{$option}";
+            echo "<li><input type='radio' name='{$index}' value='{$i}'/>{$option}";
             echo '</ul>';
         }
-
-        echo "<input type='submit' name='sub' value='Submit'/></form>";
+        echo "<input type='submit' value='Submit'/></form>";
         
-
+ 
+ 
+		foreach($_POST as $item){
+    	echo $item . "\n"; 
+		}
+ 
+		if (count($_POST) > 0) {
+         //show answers
+        } else {
+         // show form
+        }
     ?>
